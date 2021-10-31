2d1bb90370ee85e9e2805adf5280d2578515012e
Ruslan <rbruskov@mail.ru>
2021-11-01 00:33:32 +0300

    Part f HW practice_1

---
+++ HW_practice_1/Program.cs
@@ -0,0 +1,127 @@
+﻿// формирование массива  
+
+int[] createarray(int count, int indexmin, int indexmax)
+{
+
+    int index = 0;
+    int[] array = new int[count];
+    while (index < count)
+    {
+        array[index] = new Random().Next(indexmin, indexmax);
+        index++;
+    }
+    return array;
+}
+
+// отображение массива
+void showarray(int[] name)
+{
+    int index = 0;
+    int length = name.Length;
+    Console.Write("Массив сформирован:");
+    while (index < length)
+    {
+        Console.Write(" {0}", name[index]);
+        index++;
+    }
+    Console.WriteLine();
+}
+
+//проверка массива на возрастание
+
+void fromDowntoUp(int[] name)
+{
+    int index = 0;
+    int length = name.Length;
+    int max = 0;
+    while (index < length)
+    {
+        if (max < name[index])
+        {
+            Console.WriteLine(name[index]);
+            max = name[index];
+            //Console.WriteLine(max);  
+        }
+        index++;
+    }
+}
+
+//проверка кол-ва элеиентов массива между макс и мин
+
+//среднее значение элементов массива
+
+int medium(int[] name)
+{
+    int index = 0;
+    int length = name.Length;
+    int sum = 0;
+    while (index < length)
+    {
+        sum = sum + name[index];
+        index++;
+    }
+    int max = sum / length;
+    Console.WriteLine("Сумма всех элементов массива: {0}, среднее значение элемента массива: {1}", sum, max);
+    return max;
+}
+
+//проверка элементов массива, которые больше среднеарифмитического элементов массива
+
+void DownUpmed(int[] name, int max)
+{
+    int index = 0;
+    int length = name.Length;
+
+    while (index < length)
+    {
+        if (max < name[index])
+        {
+            Console.WriteLine(name[index]);
+
+            //Console.WriteLine(max);  
+        }
+        index++;
+    }
+}
+
+//четные элементы массива
+
+void evennum(int[] name)
+{
+    int index = 0;
+    int length = name.Length;
+    Console.WriteLine("четные элементы массива:");
+    while (index < length)
+    {
+       if(name[index]%2!=1)
+       {
+       Console.WriteLine(name[index]); 
+       }
+        index++;
+    }
+}
+
+//тело программы
+Console.Clear();
+Console.Write("введите длину массива:");
+int arrnum = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("введите мин. значение диапазона:");
+int indexmin = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("введите максимальное значение диапазона:");
+int indexmax = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Длина массива: {0}, диапазон [{1} : {2})", arrnum, indexmin, indexmax);
+
+
+int[] arr = createarray(arrnum, indexmin, indexmax);
+showarray(arr);
+fromDowntoUp(arr);
+int max = medium(arr);
+DownUpmed(arr, max);
+evennum(arr);
+
+// int count = 10;
+// int[] arr = createarray(count);
+// Console.WriteLine(arr[0]);
