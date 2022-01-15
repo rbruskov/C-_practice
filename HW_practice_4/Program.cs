24f136908a7c844e94458f775a3fd7f7d412eeb4
Ruslan <rbruskov@mail.ru>
2022-01-16 00:28:18 +0300

    diagram added

---
+++ HW_practice_4/Program.cs
@@ -0,0 +1,30 @@
+﻿int[,] matrix = new int[4, 4];
+
+void showarray(int[,] matr)
+{
+
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]}");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+void fillarray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 100);
+        }    
+    }
+}
+
+showarray (matrix);
+fillarray (matrix);
+showarray (matrix);
