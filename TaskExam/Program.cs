142a3a7ddf9122f2d2d4f868c0926c3677801ee4
Ruslan <rbruskov@mail.ru>
2022-01-17 01:55:12 +0300

    check corret userimput addedm

--- TaskExam/Program.cs
+++ TaskExam/Program.cs
@@ -5,11 +5,59 @@
 
 
 //ввод числа вручную
-int imput()
+string imput()
 {
     Console.WriteLine("Введите целое число");
-    int N = Convert.ToInt32(Console.ReadLine());
+    string N =(Console.ReadLine());
     return N;
+}
+
+// проверка правильности ввода данных пользователем
+
+string correctimput (int typedata, string userimp)
+{
+int a = typedata;
+string correct = string.Empty;
+
+while (a!=1)
+{
+    string userimput=imput();
+    a=whatuserEntered(userimput);
+    userimp=userimput;
+}
+correct=userimp;
+return correct;
+}
+
+// определение типа вводимых данных
+
+int whatuserEntered(string userimp) 
+{
+string userimput=userimp;
+int tempInt;
+decimal tempDec;
+int a=0;
+int usernumber=0;
+
+if(int.TryParse(userimput, out tempInt))
+  {
+    //   Console.WriteLine("целое число"); 
+      a=1;
+  
+  }
+else 
+if (decimal.TryParse(userimput, out tempDec))
+ {
+     Console.WriteLine("десятичное");
+ a=2;
+ }
+else 
+{
+Console.WriteLine("символы");
+a=3;
+}
+return a; 
+
 }
 
 // формирование массива случайными числами заданного интервала  
@@ -28,11 +76,12 @@ int[] createarray(int count, int indexmin, int indexmax)
 }
 
 // отображение массива
-void showarray(int[] name,int length)
+void showarray(int[] name,int length, string Text)
 {
     int index = 0;
+    string text1 = Text;
     // int length = name.Length;
-    Console.Write("Массив сформирован:");
+    Console.Write(text1);
     while (index < length)
     {
         Console.Write(" {0}", name[index]);
@@ -41,32 +90,62 @@ void showarray(int[] name,int length)
     Console.WriteLine();
 }
 
-//тело программы
+//?**************тело программы***************
 
 Console.Clear();
-
 Console.WriteLine("программа из массива случайных целых чисел формирует массив из четных чисел");
 
-Console.WriteLine("Введите длину массива из случайных чисел");
+Console.WriteLine("Введите длину массива для случайных чисел");
+string arraylength = imput();
+ // проверка корректного ввода данных 
+int a = whatuserEntered(arraylength); //определение типа введенных данных
+// Console.WriteLine(a);
+string arraylengthcorrect = correctimput(a,arraylength); //повтор ввода,если данные введены неверно
+int arraylengthconvert=Convert.ToInt32(arraylengthcorrect);
+//Console.WriteLine(arraylengthconvert);
 
-int arraylength = imput();
+if (arraylengthconvert<0) // проверка на случай ввода отрицательного значения
+{
+    arraylengthconvert=arraylengthconvert*(-1);
+    Console.WriteLine("вы ввели отрицательное значение данные будут взяты по модулю [{0}]" , arraylengthconvert);
+}
+//Console.WriteLine(arraylengthconvert);
 
 Console.WriteLine("введите значение начала для диапазона случайных чисел");
+string Nmin = imput();
+// проверка корректного ввода данных
+int b = whatuserEntered(Nmin);
+// Console.WriteLine(a);
+string Nmincorrect = correctimput(b,Nmin);
+int Nminconvert=Convert.ToInt32(Nmincorrect);
+//Console.WriteLine(Nminconvert);
 
-int Nmin = imput();
+Console.WriteLine("Введите конечное значение для диапазона");
+string Nmax = imput();
+//проверка корректного ввода данных
+// int c = whatuserEntered(Nmax);
+// Console.WriteLine(c);
+// string Nmaxcorrect = correctimput(c,Nmax);
+int Nmaxconvert=Convert.ToInt32(correctimput(whatuserEntered(Nmax),Nmax)); //Nmaxcorrect);
+//Console.WriteLine(Nmaxconvert);
+if (Nmaxconvert<Nminconvert) // проверка ввода корректного диапазона
+{
+int temp = 0;
+temp = Nmaxconvert;
+Nmaxconvert = Nminconvert;
+Nminconvert =temp;
+Console.WriteLine("введено значение меньше начала диапазона, выбранный диапазон скорректирован [{0}:{1}]",Nminconvert,Nmaxconvert);
+}
 
-Console.WriteLine("Введите конечное значение для диапозона");
 
-int Nmax = imput();
-
-Console.WriteLine(arraylength);
-
-int[] array = createarray(arraylength, Nmin, Nmax);
-int[] arrayeven = createarray(arraylength, 0, 0);
+// формирование массива случайных чисел по введенным значениям
+int[] array = createarray(arraylengthconvert, Nminconvert, Nmaxconvert);
+int[] arrayeven = createarray(arraylengthconvert, 0, 0);
 int j = 0;
-showarray(array,arraylength);
+showarray(array,arraylengthconvert,"массив случайных чисел сформирован");
 
-for (int i = 0; i < arraylength; i++) 
+//формирование масиива из четных данных
+for (int i = 0; i < arraylengthconvert; i++) 
 {
     if (array[i]%2==0)
     {
@@ -74,5 +153,4 @@ for (int i = 0; i < arraylength; i++)
         j++;
     }
 }
-
-showarray(arrayeven,j); 
+showarray(arrayeven,j,"массив из четных чисел сформирован"); 
